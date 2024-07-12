0e4d2be688dd64c8cf78306e975968fe2f863ce4
Gus <151650535+GusFDLPBarcelona@users.noreply.github.com>
2024-07-12 17:57:25 +0200

    Creación del servidor de autenticación. Estilos de detalle naves y otros estilos.

--- starwars/src/app/components/home/home.component.ts
+++ starwars/src/app/components/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { ListaNavesComponent } from '../lista-naves/lista-naves.component';
 import { HeaderComponent } from "../header/header.component";
 import { DetalleNavesComponent } from '../detalle-naves/detalle-naves.component';
+import { LoginComponent } from '../login/login.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ListaNavesComponent, HeaderComponent, DetalleNavesComponent],
+  imports: [ListaNavesComponent, HeaderComponent, DetalleNavesComponent, LoginComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
