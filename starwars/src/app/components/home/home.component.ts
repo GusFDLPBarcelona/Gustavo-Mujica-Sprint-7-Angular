0b98a4c6a5f74f1de763b008789c7ea953d65cc5
Gus <151650535+GusFDLPBarcelona@users.noreply.github.com>
2024-07-15 13:59:45 +0200

    función stars-field y estilos de home component.

--- starwars/src/app/components/home/home.component.ts
+++ starwars/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { ListaNavesComponent } from '../lista-naves/lista-naves.component';
 import { HeaderComponent } from "../header/header.component";
 import { DetalleNavesComponent } from '../detalle-naves/detalle-naves.component';
@@ -12,27 +12,38 @@ import { LoginComponent } from '../login/login.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    this.createStars();
+  }
+
+  createStars(): void {
+    const numStars: number = 1000;
+
+    function getRandomPosition(): [number, number] {
+      const y: number = window.innerHeight;
+      const x: number = window.innerWidth;
+      const randomY: number = Math.floor(Math.random() * y);
+      const randomX: number = Math.floor(Math.random() * x);
+      return [randomY, randomX];
+    }
+    for (let i = 0; i < numStars; i++) {
+      let star: HTMLDivElement = document.createElement("div");
+      star.className = "star";
+      const [top, left] = getRandomPosition();
+      star.style.top = `${top}px`;
+      star.style.left = `${left}px`;
+      document.body.appendChild(star);
+    }
+  }
 }
 
-const numStars: number = 100;
 
 
-function getRandomPosition(): [number, number] {
-  const y: number = window.innerHeight;
-  const x: number = window.innerWidth;
-  const randomY: number = Math.floor(Math.random() * y);
-  const randomX: number = Math.floor(Math.random() * x);
-  return [randomY, randomX];
-}
 
 
-for (let i = 0; i < numStars; i++) {
-  let star: HTMLDivElement = document.createElement("div");
-  star.className = "star";
-  const [top, left] = getRandomPosition();
-  star.style.top = `${top}px`;
-  star.style.left = `${left}px`;
-  document.body.appendChild(star);
-}
+
+
